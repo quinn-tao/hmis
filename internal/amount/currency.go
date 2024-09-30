85aae9b48bbe938bd05e78c2dcaaa3ce15de88a2
quinn-tao <t.quinn.t.dev@gmail.com>
2024-09-30 16:19:54 -0700

    [amount] Add currency type
    Add a currency type that supports yaml marshalling

---
+++ internal/amount/currency.go
@@ -0,0 +1,9 @@
+package amount
+
+import "golang.org/x/text/currency"
+
+type Currency currency.Unit
+
+func (c *Currency) MarshalYAML() (interface{}, error) {
+    return currency.Unit(*c).String(), nil
+}
